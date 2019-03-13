985d34fef3c1f9c0f8cfa4e8299e76e973cf1790
Nikolai Iakovlev <deylak@yandex.ru>
2019-03-13 17:07:18 +0300

    Layouts build fix 2

--- scripts/build-gulp.js
+++ scripts/build-gulp.js
@@ -28,6 +28,7 @@ webpack({
     alias: {
       '$trood/componentLibraries/manifest': paths.appSrc + '/config.js',
       '$trood/businessObjects/manifest': paths.appSrc + '/config.js',
+      '$trood/layouts/manifest': paths.appSrc + '/config.js',
       $trood: paths.appSrc,
     },
   },
