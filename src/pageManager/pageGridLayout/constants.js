6d4c763ddf4905ebdabb2747e829591f3b27959a
Nileff <krot.odessa.ne@ua.fm>
2019-08-15 16:43:51 +0300

    pageGridLayout padding on cssVars

--- src/pageManager/pageGridLayout/constants.js
+++ src/pageManager/pageGridLayout/constants.js
@@ -1,7 +1,10 @@
+import cssVars from '$trood/styles/variables.css'
+
+
 export const GRID_COLUMNS = 3
 export const DEFAULT_SPAN = 1
 
-export const GRID_MARGIN = 20
-export const TROOD_PAGE_PADDING = 60
+export const GRID_MARGIN = cssVars.troodGridGap
+export const TROOD_PAGE_PADDING = cssVars.troodPagePadding
 
 export const GRID_COMPONENT_TYPE = '$TROOD_GRID'
