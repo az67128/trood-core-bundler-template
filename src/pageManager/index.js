cf6c89cdeecbe8cdc60de8713ec4a31a72af0988
Nileff <krot.odessa.ne@ua.fm>
2019-12-27 14:15:06 +0300

    fix

--- src/pageManager/index.js
+++ src/pageManager/index.js
@@ -2,5 +2,7 @@ export * from './components'
 export * from './constants'
 export * from './helpers'
 
+export getPageChildContainer from './pageGridLayout/getPageChildContainer'
+
 export withFormUrlSync from './withFormUrlSync'
 export withUrlFormSync from './withUrlFormSync'
--- src/pageManager/pageGridLayout/index.js
+++ src/pageManager/pageGridLayout/index.js
@@ -1,5 +1,4 @@
 import getPageContainer from './getPageContainer'
-import getPageChildContainer from './getPageChildContainer'
 
 import { getPageId, getPageConfig, getPageBaseId, getModelIdSelector } from '../constants'
 
@@ -19,7 +18,6 @@ export default {
   getPageBaseId,
   // So we don't rewrite third gridPageContainer argument
   getPageContainer: (page, entityModelName) => getPageContainer(page, entityModelName, getModelIdSelector()),
-  getPageChildContainer,
   getPageHeaderRendererConfig,
   getModelType,
   getModelIdSelector,
