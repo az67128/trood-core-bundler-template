893c37c290ea9811bbf177fdee5efb03fd23ebfe
az67128 <az67128@gmail.com>
2020-11-17 09:01:07 +0300

    TB-554 added catch and finally for actions

--- src/core/BaseComponent/index.js
+++ src/core/BaseComponent/index.js
@@ -56,11 +56,11 @@ const connectProps = (props, $data, childBaseComponent) => {
       if (typeof item === 'object' && item.$type === '$action') {
         const sequense = item.sequense ? item.sequense : [item]
         const executor = async ($event) => {
-          const connectedActions = sequense.map((actionItem) => {
-            const connectedAction = getData(actionItem.path, $data)
+          const getConnectedFunction = (path, args) => {
+            const connectedAction = getData(path, $data)
             return ($event) => {
-              const connectedArgs = actionItem.args
-                ? actionItem.args.map((param) => {
+              const connectedArgs = args
+                ? args.map((param) => {
                   return param.$type
                     ? connectProps([param], { ...$data, $event })[0]
                     : connectProps(param, { ...$data, $event })
@@ -70,19 +70,49 @@ const connectProps = (props, $data, childBaseComponent) => {
                 ? connectedAction(...Object.values(connectedArgs))
                 : undefined
             }
-          })
-          try{
-            for (const currentAction of connectedActions) {           
-              await currentAction($event)
-            }
-          } catch(error){
-            //TODO add catch action
-            console.log(error)
-          } finally{
-            //TODO add finally action
-            // console.log('finally')
           }
-         
+
+          const connectedActions = sequense.map((actionItem) => {
+            return {
+              action: getConnectedFunction(actionItem.path, actionItem.args),
+              catch: actionItem.catch
+                ? getConnectedFunction(actionItem.catch.path, actionItem.catch.args)
+                : null,
+              finally: actionItem.finally
+                ? getConnectedFunction(actionItem.finally.path, actionItem.finally.args)
+                : null,
+            }
+          })
+          try {
+            for (const currentAction of connectedActions) {
+              // execute action, try and catch of action
+              try {
+                await currentAction.action($event)
+              } catch (error) {
+                if (typeof currentAction.catch === 'function') {
+                  currentAction.catch(error)
+                } else {
+                  throw error
+                }
+              } finally {
+                if (typeof currentAction.finally === 'function') {
+                  currentAction.finally()
+                }
+              }
+            }
+          } catch (error) {
+            if (item.sequense && item.catch) {
+              const action = getConnectedFunction(item.catch.path, item.catch.args)
+              action(error)
+            }
+            //TODO remove console
+            console.log(error)
+          } finally {
+            if (item.sequense && item.finally) {
+              const action = getConnectedFunction(item.finally.path, item.finally.args)
+              action()
+            }
+          }
         }
         return { ...memo, [key]: executor }
       }
@@ -104,7 +134,7 @@ const BaseComponent = ({ component }) => {
   const $store = React.useContext(StoreContext)
   const $page = React.useContext(PageStoreContext)
   const $context = React.useContext(ContextContext)
-  
+
   React.useEffect(() => {
     if (component) component.loadChunk()
   }, [component])
