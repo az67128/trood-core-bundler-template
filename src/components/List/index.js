851bf3044cb9dfa8ea6e064c7e4e31b47124a898
Nileff <krot.odessa.ne@ua.fm>
2020-12-11 14:21:18 +0300

    Add propTypes

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import BaseComponent from 'core/BaseComponent'
 import { Component } from 'core/pageStore'
 import Context from 'components/Context'
@@ -6,6 +7,7 @@ import Context from 'components/Context'
 import Paginator from '../internal/Paginator'
 
 const List = ({
+  className,
   entity,
   queryOptions,
   components,
@@ -14,7 +16,7 @@ const List = ({
   const componentsStore = Component.create({ components })
 
   return (
-    <Paginator {...pagination} entity={entity} queryOptions={queryOptions}>
+    <Paginator {...pagination} className={className} entity={entity} queryOptions={queryOptions}>
       {({ items }) => items.map((item) => (
         <Context key={item.id} context={item}>
           <BaseComponent  $context={item} component={componentsStore} />
@@ -22,6 +24,36 @@ const List = ({
       ))}
     </Paginator>
   )
+}
+
+List.propTypes = {
+  className: PropTypes.string,
+  entity: PropTypes.shape({
+    getPage: PropTypes.func,
+    getPageLoading: PropTypes.func,
+    getPagesCount: PropTypes.func,
+    getInfinityPages: PropTypes.func,
+    getInfinityPagesLoading: PropTypes.func,
+    getInfinityNextPage: PropTypes.func,
+    getInfinityNextPageNumber: PropTypes.func,
+  }).isRequired,
+  queryOptions: PropTypes.shape({
+    headers: PropTypes.object,
+    hash: PropTypes.string,
+    cacheMaxAgeMs: PropTypes.number,
+    filters: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  }),
+  components: PropTypes.arrayOf(PropTypes.object),
+  pagination: PropTypes.shape({
+    type: PropTypes.oneOf(['classic', 'infinity', 'disabled']),
+    defaultPageSize: PropTypes.number,
+    pagesControlsCount: PropTypes.number,
+    pageSizes: PropTypes.arrayOf(PropTypes.number),
+    scrollContainerSelector: PropTypes.string,
+    topControls: PropTypes.oneOfType([PropTypes.bool, PropTypes.arrayOf(PropTypes.object)]),
+    bottomControls: PropTypes.oneOfType([PropTypes.bool, PropTypes.arrayOf(PropTypes.object)]),
+    infinityControls: PropTypes.oneOfType([PropTypes.bool, PropTypes.arrayOf(PropTypes.object)]),
+  }),
 }
 
 export default List
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import classNames from 'classnames'
 import BaseComponent from 'core/BaseComponent'
 import { Component } from 'core/pageStore'
 import Context from 'components/Context'
@@ -7,6 +6,7 @@ import Context from 'components/Context'
 import Paginator from '../internal/Paginator'
 
 import styles from './index.module.css'
+import PropTypes from 'prop-types'
 
 
 const getComponents = (columnComponents, componentKey = 'bodyCell', wrapper = 'td') =>
@@ -34,9 +34,9 @@ const Table = ({
   const bodyComponentsStore = Component.create({ components: bodyComponents })
 
   return (
-    <Paginator {...pagination} entity={entity} queryOptions={queryOptions}>
+    <Paginator {...pagination} className={className} entity={entity} queryOptions={queryOptions}>
       {({ items }) => (
-        <table className={classNames(styles.table, className)}>
+        <table className={styles.table}>
           <thead>
             <tr>
               <BaseComponent component={headerComponentsStore} />
@@ -55,6 +55,39 @@ const Table = ({
       )}
     </Paginator>
   )
+}
+
+Table.propTypes = {
+  className: PropTypes.string,
+  entity: PropTypes.shape({
+    getPage: PropTypes.func,
+    getPageLoading: PropTypes.func,
+    getPagesCount: PropTypes.func,
+    getInfinityPages: PropTypes.func,
+    getInfinityPagesLoading: PropTypes.func,
+    getInfinityNextPage: PropTypes.func,
+    getInfinityNextPageNumber: PropTypes.func,
+  }).isRequired,
+  queryOptions: PropTypes.shape({
+    headers: PropTypes.object,
+    hash: PropTypes.string,
+    cacheMaxAgeMs: PropTypes.number,
+    filters: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  }),
+  columnComponents: PropTypes.arrayOf(PropTypes.shape({
+    headerCell: PropTypes.object,
+    bodyCell: PropTypes.object,
+  })),
+  pagination: PropTypes.shape({
+    type: PropTypes.oneOf(['classic', 'infinity', 'disabled']),
+    defaultPageSize: PropTypes.number,
+    pagesControlsCount: PropTypes.number,
+    pageSizes: PropTypes.arrayOf(PropTypes.number),
+    scrollContainerSelector: PropTypes.string,
+    topControls: PropTypes.oneOfType([PropTypes.bool, PropTypes.arrayOf(PropTypes.object)]),
+    bottomControls: PropTypes.oneOfType([PropTypes.bool, PropTypes.arrayOf(PropTypes.object)]),
+    infinityControls: PropTypes.oneOfType([PropTypes.bool, PropTypes.arrayOf(PropTypes.object)]),
+  }),
 }
 
 export default Table
