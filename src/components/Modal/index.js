a8fe69e15aeb9fee2aea817bb3c54e02e581c2e3
Nileff <krot.odessa.ne@ua.fm>
2020-12-14 16:47:13 +0300

    Modals

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,17 +1,49 @@
 import React, { useRef } from 'react'
-import './style.css'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
-const Modal = ({ isOpen, children, close, closeOnOverlayClick = true, className = '' }) => {
+import Block from '../Block'
+
+import styles from './index.module.css'
+
+
+const Modal = ({
+  className,
+  width = 320,
+  type = 'center',
+  isOpen,
+  close,
+  closeOnOverlayClick = true,
+  children,
+}) => {
   const overlayRef = useRef()
   if (!isOpen) return null
   const onOverlayClick = (event) => {
     if (closeOnOverlayClick && event.target === overlayRef.current) close()
   }
+
+  const style = {
+    width: type === 'full' ? '100%' : width,
+    height: type === 'full' ? '100%' : undefined,
+  }
+
   return (
-    <div className="overlay" onClick={onOverlayClick} ref={overlayRef}>
-      <div className={`modal ${className}`}>{children}</div>
+    <div className={classNames(styles.overlay, styles[type])} onClick={onOverlayClick} ref={overlayRef}>
+      <Block style={style} className={classNames(styles.modal, className, styles[type])}>
+        {children}
+      </Block>
     </div>
   )
+}
+
+Modal.propTypes = {
+  className: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  type: PropTypes.oneOf(['full', 'center', 'left', 'right']),
+  isOpen: PropTypes.bool,
+  close: PropTypes.func,
+  closeOnOverlayClick: PropTypes.bool,
+  children: PropTypes.node,
 }
 
 export default Modal
